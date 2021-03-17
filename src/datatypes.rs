e1bea0ba8c94e458889c4408f65459fcb2b53bfb
Anshuman <amedhi@connect.ust.hk>
2021-03-17 18:45:02 +0800

    grid cell constants

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -5,6 +5,19 @@ pub type PoseCovariance<F> = nalgebra::base::Matrix3<F>;
 pub type Point<F> = nalgebra::geometry::Point2<F>;
 pub type PointCovariance<F> = nalgebra::base::Matrix2<F>;
 
+const GRID_OCCU: i8 = 1;
+const GRID_FREE: i8 = -1;
+const GRID_NONE: i8 = 0;
+pub const fn grid_occupied(x: i8) -> bool {
+    x > GRID_NONE
+}
+pub const fn grid_mapped(x: i8) -> bool {
+    x != GRID_NONE
+}
+pub const fn grid_freecell(x: i8) -> bool {
+    x < GRID_NONE
+}
+
 pub type GridMap = nalgebra::DMatrix<i8>;
 pub type GridMapView<'a> = nalgebra::DMatrixSlice<'a, i8>;
 pub type GridMapViewMut<'a> = nalgebra::DMatrixSliceMut<'a, i8>;
