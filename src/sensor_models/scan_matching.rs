1b48baa511d22b9015c64bcf2662ec905287beb4
Anshuman <amedhi@connect.ust.hk>
2021-06-06 15:42:25 +0800

    rust libraries for NNLSQ

--- src/sensor_models/scan_matching.rs
+++ src/sensor_models/scan_matching.rs
@@ -1,2 +1,6 @@
 // TODO: laser to laser and laser to map scan matching
 // with various norms for estimating probability
+
+// https://crates.io/crates/varpro
+// https://github.com/rust-cv/levenberg-marquardt
+// https://docs.rs/GSL/0.4.28/rgsl/types/multifit_solver/index.html
